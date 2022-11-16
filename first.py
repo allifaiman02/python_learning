cef2b278a770f27bed3d1a12c084d98bdcd3d3b1
Allif Aiman <allifaiman0212@gmail.com>
2022-11-16 14:21:33 +0800

    simple

--- first.py
+++ first.py
@@ -2,8 +2,10 @@
 print("Hello World")
 myName = input("What is your name ")
 print("It is good to know you " + myName)
+
 print("The length of your name is : ")
 print(len(myName))
-myAge = input("What is your age?")
+
+myAge = input("What is your age? ")
 print("You will be " + str(int(myAge) + 1) + " in a year")
 
