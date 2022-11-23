164d0e7a7db2940e41605bab254e5f84e683c1db
Allif Aiman <allifaiman0212@gmail.com>
2022-11-23 12:37:13 +0800

    -

--- first.py
+++ first.py
@@ -4,7 +4,7 @@ myName = input("What is your name ")
 print("It is good to know you " + myName)
 
 print("The length of your name is : ")
-print(len(myName))
+print(len(myName)) 
 
 myAge = input("What is your age? ")
 print("You will be " + str(int(myAge) + 1) + " in a year")
